7ffb2f83c2eb5e159be5a340f84b7352eedae707
Mihai Marinescu <mihai.marinescu@endava.com>
2017-09-26 18:03:18 +0300

    The full example

--- example.js
+++ example.js
@@ -1,27 +1,43 @@
+// vue v2.x
 import axios from 'axios'
 
 let allUsers = [{'body': 'something'}, {'body': 'something else'}]
 async function getUsers () {
-//    const myUsers = await axios.get('https://jsonplaceholder.typicode.com/posts')
-const myUsers = await axios.get('/api/users')
-
-console.log(myUsers.data)
-this.users = myUsers.data
+    const myUsers = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    this.users = myUsers.data //calling this from outside the export, you need to bind it to the exported module's this
 }
 export default {
-name: 'hello',
-data () {
-return {
-msg: 'Welcome to Your Vue.js App',
-users: allUsers
-}
-},
-//    async created () {
-created () {
-//      allUsers = await axios.get('https://jsonplaceholder.typicode.com/posts')
-//      console.log('async stuff')
-//      this.users = allUsers.data
-//      getUsers.bind(this)()
-getUsers.call(this)
-}
+    name: 'hello',
+    data () {
+        return {
+            msg: 'The hello component',
+            users: allUsers
+        }
+    },
+    // example A
+    created () {
+        // getUsers.bind(this)() - uglier
+        getUsers.call(this)
+    }
+
+
+    // example B - with the async in front of the created keyword
+    /* async created () {
+        allUsers = await axios.get('https://jsonplaceholder.typicode.com/posts') - this is messyer
+        this.users = allUsers.data
+     } */
+
+
+    // example C - calling the get from the methods - in this case, no need to declare the function outside of the module
+    /*
+    created () {
+        this.getUsers() - from methods
+    },
+    methods: {
+        getUsers: async function() {
+            allUsers = await axios.get('https://jsonplaceholder.typicode.com/posts')
+            this.users = allUsers.data
+        }
+    }
+     */
 }
